a198b0817e410669d5c897d4219292a3874754f6
Andrezimdopvp <102673694+Andrezimdopvp@users.noreply.github.com>
2023-08-04 19:57:38 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,39 @@
+
+        const firebaseConfig = {
+          apiKey: "AIzaSyAttZQEQIxBtNblvseBjIMDpakXxl4WUtU",
+          authDomain: "vamosconversar-8c7fe.firebaseapp.com",
+          databaseURL: "https://vamosconversar-8c7fe-default-rtdb.firebaseio.com",
+          projectId: "vamosconversar-8c7fe",
+          storageBucket: "vamosconversar-8c7fe.appspot.com",
+          messagingSenderId: "415358156359",
+          appId: "1:415358156359:web:06f5f06a58b781eef6fb62"
+        };
+        
+            firebase.initializeApp(firebaseConfig);
+
+        function sair(){
+                localStorage.removeItem("Usuário");
+                window.location = "index.html";
+        }
+var nome = localStorage.getItem("Usuário");
+document.getElementById("user").innerHTML = "Bem Vindo(a) " + nome;
+
+function addRoom(){
+        var nomsala = document.getElementById("sala").value;
+        localStorage.setItem("room", nomsala);
+        firebase.database().ref("/").child(nomsala).update({
+                purpose:"Criamos Uma Pasta!"
+        });
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+        Room_names = childKey;
+        console.log("Room Name - " + Room_names);
+       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+       document.getElementById("nomesDasSalas").innerHTML += row;
+     });
+   });
+ 
+ }
+ 
+ getData();
